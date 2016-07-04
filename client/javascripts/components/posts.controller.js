43b526bbb27669810ed35b13ce8242f6e1f9f771
Kenneth Korcal <kakorcal@gmail.com>
2016-07-03 21:54:02 -0700

    making sidebar flex

--- client/javascripts/components/posts.controller.js
+++ client/javascripts/components/posts.controller.js
@@ -4,7 +4,16 @@
 
     function PostsController(PortfolioService) {
       const vm = this;
+
       vm.portfolios = PortfolioService.portfolio;
+      
+      vm.slides = vm.portfolios.reduce((acc, cur)=>{
+        for(let i = 0; i < cur.images.slideCount; i++){
+          acc.push({image: `/assets/${cur.images.path}/${i+1}.png`});
+        }
+        return acc;
+      }, []);
+      
       vm.getClass = (idx)=>{
         return idx < 3 ? `md-accent md-hue-${idx + 1}` : `md-warn md-hue-${idx - 3 + 1}`;
       };
