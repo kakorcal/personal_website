6adeeeba24426e1c83132d406649a00891f2ab79
Kenneth Korcal <kakorcal@gmail.com>
2016-07-03 18:58:16 -0700

    finish content layout. moving on to lightbox

--- client/javascripts/services.js
+++ client/javascripts/services.js
@@ -6,6 +6,7 @@
       {
         title: 'Angular Blog',
         date: 'Jun 2016',
+        desc: "Simple single page application where users can post their blogs as well as rate other user's blogs.",
         github: 'https://github.com/kakorcal/angular_blog_app',
         link: null,
         images: {path: 'angular_blog', slideCount: 3}
@@ -13,6 +14,7 @@
       {
         title: 'Knex Photo App',
         date: 'Jun 2016',
+        desc: 'Photo sharing application built with node.js. Also using passport.js for authentication and authorization',
         github: 'https://github.com/kakorcal/knex_photo_app',
         link: null,
         images: {path: 'knex_photo', slideCount: 3}
@@ -20,6 +22,7 @@
       {
         title: 'Linguo Language App',
         date: 'Apr 2016',
+        desc: "Full-stack JavaScript application that helps users find language exchange partners based on their location.",
         github: 'https://github.com/kakorcal/linguo',
         link: 'https://linguoisdead.herokuapp.com/',
         images: {path: 'linguo', slideCount: 3}
@@ -27,6 +30,7 @@
       {
         title: 'Bomberman Clone',
         date: 'Mar 2016',
+        desc: "JavaScript desktop game built with HTML5 canvas. Using the phaser.js framework for animations.",
         github: 'https://github.com/kakorcal/bomberman',
         link: 'http://house-keeper-mole-58621.bitballoon.com/',
         images: {path: 'bomberman', slideCount: 3}
@@ -34,6 +38,7 @@
       {
         title: 'Michael Baca Photography',
         date: 'Dec 2015',
+        desc: "Custom WordPress theme designed to showcase client's photo collection.",
         github: 'https://github.com/kakorcal/michael_baca',
         link: 'http://michaelbaca.com/',
         images: {path: 'michael_baca', slideCount: 3}
@@ -41,6 +46,7 @@
       {
         title: 'Sara Faith Alterman',
         date: 'Oct 2015',
+        desc: "Custom WordPress theme designed to promote client's blog posts and writings.",
         github: 'https://github.com/kakorcal/sara_alterman',
         link: 'http://sarafaithalterman.com/',
         images: {path: 'sara_alterman', slideCount: 3}
